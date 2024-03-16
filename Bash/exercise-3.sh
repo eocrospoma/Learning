eca973f8f1279b181b5268e588560855284babc0
Erick Ocrospoma <erick@ocrospoma.org>
2024-03-16 11:40:13 -0500

    Add exercise 3

---
+++ Bash/exercise-3.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Usage:
+#
+# ./exercise-3.sh sample-ex3-books.txt sample-ex3-categories.txt
+
+books=$1
+categories=$2
+
+stock_list() {
+  
+  listOfCategories=()
+  for category in `cat $categories | tr ',' ' '`
+  do
+    listOfCategories+=("$category")
+  done
+
+  first="${listOfCategories[0]}"
+  second="${listOfCategories[1]}"
+  third="${listOfCategories[2]}"
+  fourth="${listOfCategories[3]}"
+
+  cat "$books" | tr ',' '\n' | sort -k1 | awk -F ' ' \
+    -vfirst="^${first}" \
+    -vsecond="^${second}" \
+    -vthird="^${third}" \
+    -vfourth="^${fourth}" \
+    '$1 ~ first {sumFirst+=$2} \
+     $1 ~ second {sumSecond+=$2} \
+     $1 ~ third {sumThird+=$2} \
+     $1 ~ fourth {sumFourth+=$2} \
+     END \ 
+     {print "(A :"sumFirst")","- (B : "sumSecond")","- (C : "sumThird")","- (D :"sumFourth")"}'
+}
+
+stock_list "$books" "$categories"
